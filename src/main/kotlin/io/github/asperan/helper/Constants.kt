53c44d88f92bd12fd206ffcf807c7b8eb9d9cdfe
Alex Speranza <alex.speranza@studio.unibo.it>
2022-11-16 10:11:31 +0100

    feat: add constants file

---
+++ src/main/kotlin/io/github/asperan/helper/Constants.kt
@@ -0,0 +1,19 @@
+/*
+Copyright (c) 2022, Alex Speranza
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package io.github.asperan.helper
+
+/**
+ * The default priority of various elements.
+ */
+internal const val defaultPriority: UByte = 50u
+
+/**
+ * The default string to use for indentation of the xml file.
+ */
+internal const val defaultIndentString = "\t"
