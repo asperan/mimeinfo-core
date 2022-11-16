0dfbeeb3a9b0bd95a9b7dff86b2c05f6cde44de7
Alex Speranza <alex.speranza@studio.unibo.it>
2022-11-16 09:47:10 +0100

    build: add gradle properties, build and libs.versions files

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.kotlin.qa)
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+    testLogging {
+        showExceptions = true
+        showStandardStreams = true
+        events = setOf(
+            org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+        )
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    }
+}
+
+dependencies {
+    testImplementation(libs.bundles.kotest)
+}
