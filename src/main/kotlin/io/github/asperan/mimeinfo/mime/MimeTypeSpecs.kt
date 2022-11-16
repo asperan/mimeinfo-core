dc8d055a02a216d708270340edeab21b042ff43f
Alex Speranza <alex.speranza@studio.unibo.it>
2022-11-16 16:59:58 +0100

    docs: add documentation to MimeTypeSpecs

--- src/main/kotlin/io/github/asperan/mimeinfo/mime/MimeTypeSpecs.kt
+++ src/main/kotlin/io/github/asperan/mimeinfo/mime/MimeTypeSpecs.kt
@@ -12,6 +12,9 @@ import io.github.asperan.helper.builder.AbstractBuilder
 import io.github.asperan.helper.builder.BuilderStateException
 import io.github.asperan.helper.asList
 
+/**
+ * The `mime-type` element.
+ */
 class MimeTypeSpecs(
     private val type: Type,
     private val globs: List<Glob>,
@@ -56,6 +59,9 @@ class MimeTypeSpecs(
         return children.flatMap { it.map { element -> element.toXmlString(indentLevel, indentString) } }.joinToString("\n")
     }
 
+    /**
+     * Builder for MimeTypes.
+     */
     class Builder : AbstractBuilder<MimeTypeSpecs, Builder>() {
         private var type: Type? = null
         private var globs: List<Glob> = listOf()
@@ -72,19 +78,74 @@ class MimeTypeSpecs(
         private var rootXml: RootXml? = null
         private var treeMagics: List<TreeMagic> = listOf()
 
+        /**
+         * @param it The type of the MimeTypeSpecs.
+         */
         val setType = builderMethod<Type> { this.type = it}
+
+        /**
+         * @param it The glob to add to the MimeTypeSpecs.
+         */
         val addGlob = builderMethod<Glob> { this.globs += it}
+
+        /**
+         * @param it Whether to add the `glob-deleteall` element.
+         */
         val setGlobDeleteAll = builderMethod<Boolean> { this.globDeleteAll = it}
+
+        /**
+         * @param it The magic to add to the MimeTypeSpecs.
+         */
         val addMagic = builderMethod<Magic> { this.magics += it}
+
+        /**
+         * @param it Whether to add the `magic-deleteall` element.
+         */
         val setMagicDeleteAll = builderMethod<Boolean> { this.magicDeleteAll = it}
+
+        /**
+         * @param it The alias to add.
+         */
         val addAlias = builderMethod<Alias> { this.aliases += it}
+
+        /**
+         * @param it The SubClassOf to add.
+         */
         val addSuperClass = builderMethod<SubClassOf> { this.superClasses += it}
+
+        /**
+         * @param it The Comment to add.
+         */
         val addComment = builderMethod<Comment> { this.comments += it}
+
+        /**
+         * @param it The Acronym to add.
+         */
         val addAcronym = builderMethod<Acronym> { this.acronyms += it}
+
+        /**
+         * @param it The ExpandedAcronym to Add.
+         */
         val addExpandedAcronym = builderMethod<ExpandedAcronym> { this.expandedAcronyms += it}
+
+        /**
+         * @param it The Icon to set.
+         */
         val setIcon = builderMethod<Icon?> { this.icon = it}
+
+        /**
+         * @param it The generic Icon to set.
+         */
         val setGenericIcon = builderMethod<Icon?> { this.genericIcon = it}
+
+        /**
+         * @param it The RootXml to set.
+         */
         val setRootXml = builderMethod<RootXml?> { this.rootXml = it}
+
+        /**
+         * @param it The TreeMagic to add.
+         */
         val addTreeMagic = builderMethod<TreeMagic> { this.treeMagics += it}
 
         override fun build(): MimeTypeSpecs = when (this.type) {
@@ -93,11 +154,17 @@ class MimeTypeSpecs(
         }
     }
 
+    /**
+     * Type of the MimeType.
+     */
     data class Type(
         private val mimeClass: MimeClass,
         private val type: String,
     ) {
 
+        /**
+         * Mime classes. They are defined in the MIME type RFC (Section 5 of RFC 2045).
+         */
         enum class MimeClass {
             APPLICATION, AUDIO, FONT, IMAGE, MODEL, TEXT, VIDEO;
             override fun toString(): String = this.name.lowercase()
