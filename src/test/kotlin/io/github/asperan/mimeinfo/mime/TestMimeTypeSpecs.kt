aa42d92729860fcc39b112015808d0ed94d4b5e8
Alex Speranza <alex.speranza@studio.unibo.it>
2022-12-03 18:41:36 +0100

    style: fix style in test

--- src/test/kotlin/io/github/asperan/mimeinfo/mime/TestMimeTypeSpecs.kt
+++ src/test/kotlin/io/github/asperan/mimeinfo/mime/TestMimeTypeSpecs.kt
@@ -17,15 +17,15 @@ class TestMimeTypeSpecs : FunSpec() {
         test("Simple MimeTypeSpecs XML") {
             val testString = "<mime-type type=\"text/drawio\">\n" +
                 "$DEFAULT_INDENT_STRING<comment >\n" +
-                        "${DEFAULT_INDENT_STRING.repeat(2)}Just a comment\n" +
-                    "$DEFAULT_INDENT_STRING</comment>\n" +
-                    "$DEFAULT_INDENT_STRING<acronym >\n" +
-                        "${DEFAULT_INDENT_STRING.repeat(2)}DRAWIO\n" +
-                    "$DEFAULT_INDENT_STRING</acronym>\n" +
-                    "$DEFAULT_INDENT_STRING<expanded-acronym >\n" +
-                        "${DEFAULT_INDENT_STRING.repeat(2)}Drawio Project\n" +
-                    "$DEFAULT_INDENT_STRING</expanded-acronym>\n" +
-                    "$DEFAULT_INDENT_STRING<sub-class-of type=\"text/plain\"/>\n" +
+                "${DEFAULT_INDENT_STRING.repeat(2)}Just a comment\n" +
+                "$DEFAULT_INDENT_STRING</comment>\n" +
+                "$DEFAULT_INDENT_STRING<acronym >\n" +
+                "${DEFAULT_INDENT_STRING.repeat(2)}DRAWIO\n" +
+                "$DEFAULT_INDENT_STRING</acronym>\n" +
+                "$DEFAULT_INDENT_STRING<expanded-acronym >\n" +
+                "${DEFAULT_INDENT_STRING.repeat(2)}Drawio Project\n" +
+                "$DEFAULT_INDENT_STRING</expanded-acronym>\n" +
+                "$DEFAULT_INDENT_STRING<sub-class-of type=\"text/plain\"/>\n" +
                 "</mime-type>"
             val m = MimeTypeSpecs.Builder()
                 .setType(MimeTypeSpecs.Type(MimeTypeSpecs.Type.MimeClass.TEXT, "drawio"))
