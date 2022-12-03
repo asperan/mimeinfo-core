f9b9304959a99c30f4d2cbba7b11f4ed93c9bd5a
Alex Speranza <alex.speranza@studio.unibo.it>
2022-12-03 18:44:14 +0100

    style: fix style

--- src/main/kotlin/io/github/asperan/mimeinfo/mime/MimeTypeSpecs.kt
+++ src/main/kotlin/io/github/asperan/mimeinfo/mime/MimeTypeSpecs.kt
@@ -8,9 +8,9 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 package io.github.asperan.mimeinfo.mime
 
+import io.github.asperan.helper.asList
 import io.github.asperan.helper.builder.AbstractBuilder
 import io.github.asperan.helper.builder.BuilderStateException
-import io.github.asperan.helper.asList
 
 /**
  * The `mime-type` element.
@@ -30,13 +30,13 @@ class MimeTypeSpecs(
     private val genericIcon: Icon?,
     private val rootXml: RootXml?,
     private val treeMagics: List<TreeMagic>,
-) : ElementWithCompoundChildren()
-{
+) : ElementWithCompoundChildren() {
     override val elementName: String get() = "mime-type"
     override val attributesString: String get() = "type=\"${this.type}\""
 
     override fun getChildrenString(indentLevel: UInt, indentString: String): String {
-        val children: Sequence<List<MimeTypeElement>> = sequenceOf(this.comments,
+        val children: Sequence<List<MimeTypeElement>> = sequenceOf(
+            this.comments,
             this.acronyms,
             this.expandedAcronyms,
             this.superClasses,
@@ -44,12 +44,12 @@ class MimeTypeSpecs(
             this.icon.asList(),
             this.genericIcon.asList(),
             this.rootXml.asList(),
-            when(this.globDeleteAll) {
+            when (this.globDeleteAll) {
                 true -> listOf(GlobDeleteAll)
                 false -> listOf()
             },
             this.globs,
-            when(this.magicDeleteAll) {
+            when (this.magicDeleteAll) {
                 true -> listOf(MagicDeleteAll)
                 false -> listOf()
             },
@@ -76,79 +76,79 @@ class MimeTypeSpecs(
         private var acronyms: List<Acronym> = listOf()
         private var expandedAcronyms: List<ExpandedAcronym> = listOf()
         private var icon: Icon? = null
-        private var genericIcon: Icon? = null
+        private var genericIcon: GenericIcon? = null
         private var rootXml: RootXml? = null
         private var treeMagics: List<TreeMagic> = listOf()
 
         /**
          * @param it The type of the MimeTypeSpecs.
          */
-        val setType = builderMethod<Type> { this.type = it}
+        val setType = builderMethod<Type> { this.type = it }
 
         /**
          * @param it The glob to add to the MimeTypeSpecs.
          */
-        val addGlob = builderMethod<Glob> { this.globs += it}
+        val addGlob = builderMethod<Glob> { this.globs += it }
 
         /**
          * @param it Whether to add the `glob-deleteall` element.
          */
-        val setGlobDeleteAll = builderMethod<Boolean> { this.globDeleteAll = it}
+        val setGlobDeleteAll = builderMethod<Boolean> { this.globDeleteAll = it }
 
         /**
          * @param it The magic to add to the MimeTypeSpecs.
          */
-        val addMagic = builderMethod<Magic> { this.magics += it}
+        val addMagic = builderMethod<Magic> { this.magics += it }
 
         /**
          * @param it Whether to add the `magic-deleteall` element.
          */
-        val setMagicDeleteAll = builderMethod<Boolean> { this.magicDeleteAll = it}
+        val setMagicDeleteAll = builderMethod<Boolean> { this.magicDeleteAll = it }
 
         /**
          * @param it The alias to add.
          */
-        val addAlias = builderMethod<Alias> { this.aliases += it}
+        val addAlias = builderMethod<Alias> { this.aliases += it }
 
         /**
          * @param it The SubClassOf to add.
          */
-        val addSuperClass = builderMethod<SubClassOf> { this.superClasses += it}
+        val addSuperClass = builderMethod<SubClassOf> { this.superClasses += it }
 
         /**
          * @param it The Comment to add.
          */
-        val addComment = builderMethod<Comment> { this.comments += it}
+        val addComment = builderMethod<Comment> { this.comments += it }
 
         /**
          * @param it The Acronym to add.
          */
-        val addAcronym = builderMethod<Acronym> { this.acronyms += it}
+        val addAcronym = builderMethod<Acronym> { this.acronyms += it }
 
         /**
          * @param it The ExpandedAcronym to Add.
          */
-        val addExpandedAcronym = builderMethod<ExpandedAcronym> { this.expandedAcronyms += it}
+        val addExpandedAcronym = builderMethod<ExpandedAcronym> { this.expandedAcronyms += it }
 
         /**
          * @param it The Icon to set.
          */
-        val setIcon = builderMethod<Icon?> { this.icon = it}
+        val setIcon = builderMethod<Icon?> { this.icon = it }
 
         /**
          * @param it The generic Icon to set.
          */
-        val setGenericIcon = builderMethod<Icon?> { this.genericIcon = it}
+        val setGenericIcon = builderMethod<GenericIcon?> { this.genericIcon = it }
 
         /**
          * @param it The RootXml to set.
          */
-        val setRootXml = builderMethod<RootXml?> { this.rootXml = it}
+        val setRootXml = builderMethod<RootXml?> { this.rootXml = it }
 
         /**
          * @param it The TreeMagic to add.
          */
-        val addTreeMagic = builderMethod<TreeMagic> { this.treeMagics += it}
+        val addTreeMagic = builderMethod<TreeMagic> { this.treeMagics += it }
 
         override fun build(): MimeTypeSpecs = when (this.type) {
             is Type -> MimeTypeSpecs(
